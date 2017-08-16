1cbaa45233f941460649c6592b2494073e1d74ce
蒋朝任 <i@jcrspace.com>
2017-08-16 18:44:03 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <page_header></page_header>
+    </div>
+</template>
+
+<script>
+
+    import page_header from './components/header.vue'
+
+    export default {
+        components: {page_header},
+        name: 'app',
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
