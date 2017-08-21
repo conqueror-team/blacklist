eb6fc3e1a4e13a28eff8f15dc2152f89b2c0cd86
ShaoWeng <i@jcrspace.com>
2017-08-21 17:27:35 +0800

    添加content

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-    <div id="app">
+    <div id="app" class="container">
         <page_header></page_header>
+        <page_content></page_content>
     </div>
 </template>
 
 <script>
 
-    import page_header from './components/header.vue'
+    import page_header from './components/page_header.vue';
+    import page_content from './components/page_content.vue';
 
     export default {
-        components: {page_header},
+        components: {
+            page_content,
+            page_header},
         name: 'app',
         data() {
             return {
---
+++ src/components/page_content.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="bs-example">
+        <table class="table">
+            <thead>
+            <tr>
+                <th></th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th>1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th>2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th>3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'page_content',
+        data() {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+    .bs-example {
+        background-color: #fff;
+        border-color: #ddd;
+        border-width: 1px;
+        border-radius: 6px 6px 6px 6px;
+        -webkit-box-shadow: none;
+        box-shadow: none;
+        position: relative;
+        padding: 15px 15px 15px;
+        margin: 0 -15px 15px;
+        border-style: solid;
+    }
+</style>
